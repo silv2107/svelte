e112d36396412a7c56fb14d4bb01727656275aee
silv2107 <6332838@rts-365.dk>
2022-09-13 17:42:53 +0200

    styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,34 @@
     export let data
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div>
-    <ul>
+<header class="top">
+    <h1>Lighten up with a joke</h1>
+    <h3>Laughter is an instant vacation</h3>
+</header>
+<div class="jokeWrapper">
+    <ul class="jokeContainer">
         {#each data.jokes as joke }
         <!-- ${data.jokes.indexOf(joke)} -->
-        <li><a href={`/details`}>{joke.setup}</a></li>
+        <li class="listItem"><a class="link" href={`/details/${data.jokes.indexOf(joke)}`}>{joke.setup}</a></li>
         {/each}
     </ul>
 </div>
+
+<style>
+    .top {
+        text-align: center;
+    }
+    .jokeContainer {
+        display: grid;
+        grid-template-columns: repeat(2,1fr);
+    }
+    .listItem {
+        list-style: none;
+        padding: 50px;
+        text-align: center;
+        border: rgba(0, 0, 255, 0.415) 10px solid;
+    }
+    .link {
+        text-decoration: none;
+    }
+</style>
